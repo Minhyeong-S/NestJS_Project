ad31d6f9d4557021a4dfa3619758dd90099efbb0
Minhyeong <alsuddl25@icloud.com>
2023-01-27 02:04:16 +0900

    GraphQL+TypeORM 기본 CRUD

--- 2.GraphQL_API/src/app.module.ts
+++ 2.GraphQL_API/src/app.module.ts
@@ -4,8 +4,14 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PodcastsModule } from './podcast/podcasts.module';
 
+
 @Module({
-  imports: [PodcastsModule, GraphQLModule.forRoot({ autoSchemaFile: true })],
+  imports: [PodcastsModule,     GraphQLModule.forRoot({
+    
+    autoSchemaFile: true,
+    // jwt middleware에서 확인하고 req.user에 넣어준 user 정보를 resolver에서 사용할 수 있도록 해준다.
+    // context: ({ req }) => ({ user: req['user'] }),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
